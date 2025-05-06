51c0778bd36dc813959ed14453258b2169eca10c
xyliganchik2007 <yarikoval2512@mail.ru>
2025-05-06 20:41:39 +0300

    time input validation

--- src/chronoio.cpp
+++ src/chronoio.cpp
@@ -1,12 +1,21 @@
 #include "chronoio.h"
 
 std::istream &operator>>(std::istream &in, std::chrono::time_point<std::chrono::system_clock> &time_point) {
+    std::string s;
+    in >> s;
+    if (s.size() != 5 || s[2] != ':' ||
+        !std::isdigit(s[0]) || !std::isdigit(s[1]) ||
+        !std::isdigit(s[3]) || !std::isdigit(s[4])) {
+        throw std::runtime_error("Couldn't parse time");
+    }
+    std::stringstream ss(s);
     std::tm tm{};
     tm.tm_year = 70;
     tm.tm_mon = 0;
     tm.tm_mday = 1;
-    in >> std::get_time(&tm, TIME_FMT.c_str());
-    if (in.fail() || tm.tm_hour >= 24 || tm.tm_min >= 60) {
+    tm.tm_hour = std::stoi(s.substr(0, 2));
+    tm.tm_min = std::stoi(s.substr(3, 2));
+    if (in.fail() || tm.tm_hour < 0 || tm.tm_hour >= 24 || tm.tm_min < 0 || tm.tm_min >= 60) {
         throw std::runtime_error("Couldn't parse time");
     }
     time_point = std::chrono::system_clock::from_time_t(std::mktime(&tm));
